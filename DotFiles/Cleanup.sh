e40ceb19f6c1274d896afe9ba36044df9ac774e2
Helge Rasmussen <helge.rasmussen@bksv.com>
2020-03-03 15:36:27 +0100

    Changed Setup.sh to not contain username. Added Cleanup.sh

---
+++ DotFiles/Cleanup.sh
@@ -0,0 +1,20 @@
+DOTDIR=misc/DotFiles
+cd ~
+
+rm .bash_aliases
+rm .bash_git
+rm .bash_logout
+rm .bash_profile
+rm .bashrc
+rm .config
+rm .gdbinit
+rm .gitconfig
+rm .inputrc
+rm .profile
+rm .tmux
+rm .tmux.conf
+rm .vim
+rm .viminfo
+rm .xmonad
+rm .Xresources
+rm .fonts
--- DotFiles/Setup.sh
+++ DotFiles/Setup.sh
@@ -1,18 +1,20 @@
-DOTDIR=/home/pi/misc/DotFiles
-ln $DOTDIR/.bash_aliases ~/.bash_aliases
-ln $DOTDIR/.bash_git ~/.bash_git
-ln $DOTDIR/.bash_logout ~/.bash_logout
-ln $DOTDIR/.bash_profile ~/.bash_profile
-ln $DOTDIR/.bashrc ~/.bashrc
-ln -s $DOTDIR/.config ~/.config
-ln $DOTDIR/.gdbinit ~/.gdbinit
-ln $DOTDIR/.gitconfig ~/.gitconfig
-ln $DOTDIR/.inputrc ~/.inputrc
-ln $DOTDIR/.profile ~/.profile
-ln -s $DOTDIR/.tmux ~/.tmux
-ln $DOTDIR/.tmux.conf ~/.tmux.conf
-ln -s $DOTDIR/.vim ~/.vim
-ln $DOTDIR/.viminfo ~/.viminfo
-ln -s $DOTDIR/.xmonad ~/.xmonad
-ln $DOTDIR/.Xresources ~/.Xresources
-ln -s $DOTDIR/.fonts ~/.fonts
+DOTDIR=misc/DotFiles
+cd ~
+
+ln $DOTDIR/.bash_aliases .
+ln $DOTDIR/.bash_git .
+ln $DOTDIR/.bash_logout .
+ln $DOTDIR/.bash_profile .
+ln $DOTDIR/.bashrc .
+ln -s $DOTDIR/.config .
+ln $DOTDIR/.gdbinit .
+ln $DOTDIR/.gitconfig .
+ln $DOTDIR/.inputrc .
+ln $DOTDIR/.profile .
+ln -s $DOTDIR/.tmux .
+ln $DOTDIR/.tmux.conf .
+ln -s $DOTDIR/.vim .
+ln $DOTDIR/.viminfo .
+ln -s $DOTDIR/.xmonad.
+ln $DOTDIR/.Xresources .
+ln -s $DOTDIR/.fonts .
