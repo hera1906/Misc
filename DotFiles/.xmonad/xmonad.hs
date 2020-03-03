b75429c541e648fe7b4ab4972fbaddf87309abe1
Helge Rasmussen <helge.rasmussen@bksv.com>
2020-03-03 06:19:43 +0100

    Changed M-d to open dmenu

--- DotFiles/.xmonad/xmonad.hs
+++ DotFiles/.xmonad/xmonad.hs
@@ -249,7 +249,7 @@ myKeys =
         , ("M-<Return>", spawn myTerminal)
 
     --- Dmenu Scripts (Alt+Ctr+Key)
-        , ("M-d-<Return>", spawn "dmenu_run -fn 'UbuntuMono Nerd Font:size=10' -nb '#292d3e' -nf '#bbc5ff' -sb '#82AAFF' -sf '#292d3e' -p 'dmenu:'")
+        , ("M-d", spawn "dmenu_run -fn 'UbuntuMono Nerd Font:size=10' -nb '#292d3e' -nf '#bbc5ff' -sb '#82AAFF' -sf '#292d3e' -p 'dmenu:'")
     ---    , ("M1-C-<Return>", spawn "dmenu_run -fn 'UbuntuMono Nerd Font:size=10' -nb '#292d3e' -nf '#bbc5ff' -sb '#82AAFF' -sf '#292d3e' -p 'dmenu:'")
     ---    , ("M1-C-e", spawn "./.dmenu/dmenu-edit-configs.sh")
     ---    , ("M1-C-h", spawn "./.dmenu/dmenu-hugo.sh")
